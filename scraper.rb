2b2e16c791ae0327c4d5c75e691976a7f837bf63
Tony Bowden <tony@tmtm.com>
2018-04-13 07:43:16 +0100

    Remove Wikipedia scraping
    The Wikipedia page has been removed. We _could_ get that data back from
    pointing at the last version of it before the redirect was added, but
    ideally all the people we need _should_ be found from the SPARQL query
    anyway. If they're not currently being returned from that, we should add
    relevant P39s.

--- scraper.rb
+++ scraper.rb
@@ -4,12 +4,7 @@
 require 'wikidata/fetcher'
 require 'pry'
 
-names = EveryPolitician::Wikidata.wikipedia_xpath(
-  url:   'https://en.wikipedia.org/wiki/List_of_current_members_of_the_National_Assembly_of_Malawi',
-  xpath: '//table[tr[th[.="Party"]]]//td[1]//a[not(@class="new")]/@title'
-)
-
 sparq = 'SELECT DISTINCT ?item WHERE { ?item p:P39 [ ps:P39 wd:Q21295990 ; pq:P2937 wd:Q45767574 ] }'
 ids = EveryPolitician::Wikidata.sparql(sparq)
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: names })
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids)
