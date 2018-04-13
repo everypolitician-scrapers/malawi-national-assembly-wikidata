3908dbe371844b93bf109a762509359c2a9a6ee6
Tony Bowden <tony@tmtm.com>
2018-04-13 07:38:18 +0100

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,12 +1,12 @@
 #!/bin/env ruby
-# encoding: utf-8
+# frozen_string_literal: true
 
 require 'wikidata/fetcher'
 require 'pry'
 
 names = EveryPolitician::Wikidata.wikipedia_xpath(
-  url: 'https://en.wikipedia.org/wiki/List_of_current_members_of_the_National_Assembly_of_Malawi',
-  xpath: '//table[tr[th[.="Party"]]]//td[1]//a[not(@class="new")]/@title',
+  url:   'https://en.wikipedia.org/wiki/List_of_current_members_of_the_National_Assembly_of_Malawi',
+  xpath: '//table[tr[th[.="Party"]]]//td[1]//a[not(@class="new")]/@title'
 )
 
 sparq = 'SELECT DISTINCT ?item WHERE { ?item p:P39 [ ps:P39 wd:Q21295990 ; pq:P2937 wd:Q45767574 ] }'
